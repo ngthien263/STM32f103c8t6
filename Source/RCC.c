e1fb709daa30501f47bc40ee6b2df96f3f69fbd0
unknown <vuongngocthien222@gmail.com>
2024-09-05 10:06:33 +0700

    fix GetSysClk

--- Source/RCC.c
+++ Source/RCC.c
@@ -14,14 +14,14 @@ uint32_t RCC_GetSysClk()
         freq =  HSE_CLK;
     } 
     else if (RCC->CFGR.BITS.SWS == SWS_PLL && RCC->CR.BITS.PLLON == 1) {
-				if(RCC->CFGR.BITS.PLLSRC){
-						static const uint8_t  PLLMULFactorTable[16U] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16};
-						freq = (RCC->CFGR.BITS.PLLXTPRE) ? (HSE_CLK >> 1) : HSE_CLK;
-						freq *= PLLMULFactorTable[(uint32_t)RCC->CFGR.BITS.PLLMUL];
+			  static const uint8_t  PLLMULFactorTable[16U] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16};
+				if(RCC->CFGR.BITS.PLLSRC){		
+						freq = (RCC->CFGR.BITS.PLLXTPRE) ? (HSE_CLK >> 1) : HSE_CLK;		
 				}
 				else{
 						freq = 4000000ul;
 				}
+				freq *= PLLMULFactorTable[(uint32_t)RCC->CFGR.BITS.PLLMUL];
     } 
     else {
         return -1;
@@ -31,7 +31,7 @@ uint32_t RCC_GetSysClk()
 
 uint32_t RCC_GetHClk()
 {		
-    return RCC_GetSysClk()/AHBpreFactorTable[RCC->CFGR.BITS.HPRE];
+    return RCC_GetSysClk() / AHBpreFactorTable[RCC->CFGR.BITS.HPRE];
 }
 
 uint32_t RCC_GetAbp1Clk()
